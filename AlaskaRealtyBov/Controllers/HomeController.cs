2aecce05ea5f9cdb7f09d4c20870689695eae5b5
jessjack33 <95002971+jessjack33@users.noreply.github.com>
2025-08-10 12:50:59 -0700

    Initial commit

---
+++ AlaskaRealtyBov/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using AlaskaRealtyBov.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlaskaRealtyBov.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MetaKeywords = "Tribute to an Alaskan Father";
+            ViewBag.MetaDescription = "In memory of an Alaskan Father";
+            return View();
+        }
+
+        public IActionResult ReachUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
